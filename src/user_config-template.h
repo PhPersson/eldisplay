f13eb04b7e3acabfa7bfcafa75667d3112d7f857
PhPersson <51906227+PhPersson@users.noreply.github.com>
2024-10-10 00:33:13 +0200

    Updated user_config

--- src/user_config-template.h
+++ src/user_config-template.h
@@ -2,7 +2,6 @@
 #include <WString.h>
 
 
-const char* SSID = "Your SSID here";
-const char* WiFipassword = "WiFi password here";
 char* electricityPriceArea = ""; // Any of SE1, SE2, SE3 or SE4
-String api_url = "API-url to fetch the price";
+String api_url = ""; // API-url to fetch the price
+float priceThreshold = 0.5; // The threshold when price should be consider high
